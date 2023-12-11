12ae440439be157635feca5af62bcd6f1f18a205
Marhonde <SemenKuzer@mail.ru>
2023-12-11 14:38:23 +0700

    new file:   css/registrationPage.css 	modified:   index.html 	modified:   php/registration.php

--- php/registration.php
+++ php/registration.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/registrationPage.css">
     <title>Регистрация</title>
 </head>
 <body>
-    
+    <div class="back-img"></div>
+
+    <form action="" method="post">
+
+    </form>
 </body>
 </html>
