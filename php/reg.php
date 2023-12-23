95f9147751ae1ca22d8225cc347d7a3bcd5d3988
Marhonde <SemenKuzer@mail.ru>
2023-12-23 10:48:15 +0700

    modified:   html/reg/regError.html 	modified:   php/reg.php 	new file:   regCSS/regError.css

--- php/reg.php
+++ php/reg.php
@@ -1,38 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../regCSS/regStyle.css">
-    <link rel="stylesheet" href="../../regCSS/regForm.css">
-    <link rel="icon" type="image/png" href="../../img/Logo.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Регистрация аккаунта</title>
-</head>
-<body>
-    <div id="BackI"></div>
-
-    <main>
-        <div class="wrapper">
-            <h1>Давайте создадим<br> ваш аккаунт!</h1>
-    
-            <form action="../../php/reg.php" method="post">
-                <input type="text" name="login" placeholder="Логин" class="email" required>
-
-                <input type="password" name="password" placeholder="Придумайте пароль" class="password" required>
-
-                <input type="password" name="confirmPassword" placeholder="Подтвердите пароль" class="confirm-password" required>
-
-                <button type="submit" class="enter-button">Зарегистрироваться</button>
-            </form>
-            
-            <div class="have-account">
-                <p>Уже есть аккаунт? <a href="../login/login.html">Войдите тут</a></p>
-            </div>
-        </div>
-
-    </main>
-</body>
-</html> -->
 <?
 $servername = 'localhost';
 $username = 'root';
@@ -61,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             echo "Произошла ошибка! " . $conn->error;
         }
     } else {
-        echo "Пароли не совпадают!";
+        header('Location: ../html/reg/regError.html');
     }
 }
 
